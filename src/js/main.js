24d0b8ad8a07eaa016cbdee6804179e4348dbdae
LambOfGod <dvs.weter1@gmail.com>
2020-04-15 04:03:50 +0400

    САМ! Переписал появление всплывающего окна на JQuery

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,23 @@
-var button = document.querySelector('#button');
+/*var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
 
 button.addEventListener('click', function() {
   modal.classList.add('modal_active');
+  setTimeout(() => {
+    modal.classList.remove('modal_active');
+  }, 5000);
 });
 
 close.addEventListener('click', function() {
   modal.classList.remove('modal_active');
+});*/
+
+$(document).ready(function() {
+  $('#button').click(function() {
+    $('#modal').addClass('modal_active');
+  });
+  $('#close').click(function() {
+    $('#modal').removeClass('modal_active')
+  });
 });
